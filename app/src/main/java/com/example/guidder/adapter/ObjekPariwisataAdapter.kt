df320a07dc8519949c154ce2156abc916e05898e
David Christian <136609271+Myaneeeee@users.noreply.github.com>
2025-02-06 23:04:59 +0700

    Maybe list of objek pariwisata

--- app/src/main/java/com/example/guidder/adapter/ObjekPariwisataAdapter.kt
+++ app/src/main/java/com/example/guidder/adapter/ObjekPariwisataAdapter.kt
@@ -1,11 +1,43 @@
 package com.example.guidder.adapter
 
 import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.guidder.DetailActivity
+import com.example.guidder.databinding.ObjekPariwisataCardBinding
 import com.example.guidder.model.ObjekPariwisata
 
-//class ObjekPariwisataAdapter(private val context: Context, private val itemList : List<ObjekPariwisata>) : RecyclerView.Adapter<>(){
-//
-//    class ViewHolder(val context: Context, val binding: )
-//
-//}
+class ObjekPariwisataAdapter(private val context: Context, private val itemList : List<ObjekPariwisata>) : RecyclerView.Adapter<ObjekPariwisataAdapter.ViewHolder>(){
+
+    class ViewHolder(val context: Context, val binding: ObjekPariwisataCardBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            context,
+            ObjekPariwisataCardBinding.inflate(LayoutInflater.from(parent.context), parent, false),
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return itemList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val objekPariwisata = itemList[position]
+        val resourceId = context.resources.getIdentifier(objekPariwisata.gambar, "drawable", context.packageName)
+
+        holder.binding.tvNama.text = objekPariwisata.nama
+        holder.binding.ivGambar.setImageResource(resourceId)
+        holder.binding.cvObjekPariwisata.setOnClickListener {
+            val intent = Intent(holder.context, DetailActivity::class.java)
+            intent.putExtra("id_objek_wisata", objekPariwisata.id_objek_wisata)
+            context.startActivity(intent)
+        }
+    }
+
+}
--- app/src/main/java/com/example/guidder/fragment/ListObjekPariwisata.kt
+++ app/src/main/java/com/example/guidder/fragment/ListObjekPariwisata.kt
@@ -5,56 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.guidder.R
+import com.example.guidder.adapter.ObjekPariwisataAdapter
+import com.example.guidder.database.DatabaseHelper
+import com.example.guidder.databinding.FragmentListObjekPariwisataBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ListObjekPariwisata.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ListObjekPariwisata : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding : FragmentListObjekPariwisataBinding
+    private lateinit var objekPariwisataAdapter: ObjekPariwisataAdapter
+    private lateinit var databaseHelper: DatabaseHelper
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list_objek_pariwisata, container, false)
+        binding = FragmentListObjekPariwisataBinding.inflate(layoutInflater, container, false)
+
+        setUpRecycler()
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ListObjekPariwisata.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ListObjekPariwisata().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun setUpRecycler() {
+        val listObjekPariwisata = databaseHelper.getObjekPariwisata()
+        objekPariwisataAdapter = ObjekPariwisataAdapter(requireContext(), listObjekPariwisata)
+        binding.objekPariwisataList.adapter = objekPariwisataAdapter
+        binding.objekPariwisataList.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
     }
+
 }
